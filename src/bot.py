d9e183d43afe389c04bbf97769f2c50d420ed9b0
Shikha <shikhavnair@gmail.com>
2021-09-12 20:20:26 -0400

    requirements.txt and bot test

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,5 @@
 import os
-
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -9,20 +9,72 @@ import calendar
 import office_hours
 import profanity_filter
 import qna
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 
+#load_dotenv()
+#TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = 'ODg0ODU1NTgyMjI4NjI3NTI2.YTej-g.vVAhK9VWSuQm7O8pkVgssdS-bP0'
+#GUILD = 'TeachersPet-Dev'
+client = discord.Client()
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+'''
+@client.event
+async def on_ready():
+    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
 
-bot = commands.Bot(command_prefix='!')
+client.run(TOKEN)
 
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
+'''
+
+intents=discord.Intents.default()
+bot = commands.Bot(command_prefix='!', description='This is TeachersPetBot!', intents=intents)
+intents.members = True
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+'''
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
 
+    if message.content == 'hey bot':
+        response = 'hi user!!!'
+        await message.channel.send(response)
+
     # profanity_filter.check_profanity(...args)
+'''
+'''
+NOTE: bot commands don't work if client methods or bot on_message is implemented
+'''
+@bot.command()
+async def test(ctx):
+    await ctx.send('test successful')
 
 # office hour commands
 @bot.command('oh')
