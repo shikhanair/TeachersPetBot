3199cf4ab0fa86e602fdf1d5a84bbbda0f0441f5
Tanya Chu <tanya.chu0626@gmail.com>
2021-09-27 20:33:23 -0400

    Store message id and fetch, store answer #3

--- src/qna.py
+++ src/qna.py
@@ -1,10 +1,14 @@
 # Functionality related to Q & A
 from discord import NotFound
 
+# keep track of next question number
 question_number = 1
+
+# dictionary of questions with answers
 qna = {}
 
 
+# object with question details
 class QuestionsAnswers:
     def __init__(self, q, number, message, ans):
         self.question = q
@@ -18,12 +22,13 @@ async def question(ctx, q):
     global qna
     global question_number
 
+    # format question
     q_str = 'Q' + str(question_number) + ': ' + q + '\n'
 
     message = await ctx.send(q_str)
 
     # create qna object
-    new_question = QuestionsAnswers(q, question_number, message, '')
+    new_question = QuestionsAnswers(q, question_number, message.id, '')
     # add question to list
     qna[question_number] = new_question
 
@@ -38,8 +43,6 @@ async def question(ctx, q):
 async def answer(ctx, num, ans):
     global qna
 
-    print('context: \n', ctx)
-
     # check if question number exists
     if int(num) not in qna.keys():
         await ctx.author.send('Invalid question number: ' + str(num))
@@ -49,17 +52,29 @@ async def answer(ctx, num, ans):
 
     # get question
     q_answered = qna[int(num)]
-    message = q_answered.msg
+    # check if message exists
+    try:
+        message = await ctx.fetch_message(q_answered.msg)
+    except NotFound:
+        await ctx.author.send('Invalid question number: ' + str(num))
+        # delete user msg
+        await ctx.message.delete()
+        return
 
     # generate and edit msg with answer
-    content = message.content + '\n'
     if "instructor" in [y.name.lower() for y in ctx.author.roles]:
         role = 'Instructor'
     else:
         role = 'Student'
-    content = message.content + '\n' + role + ' Ans: ' + ans
+    new_answer = role + ' Ans: ' + ans
 
-    # check if message exists
+    # store new answer
+    if not q_answered.answer == '':
+        q_answered.answer += '\n'
+    q_answered.answer += new_answer
+
+    # check if message exists and edit
+    content = message.content + '\n' + new_answer
     try:
         await message.edit(content=content)
     except NotFound:
