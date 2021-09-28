829c88425ae6240a67f3222fbbecf065c5d1513b
Tanya Chu <tanya.chu0626@gmail.com>
2021-09-28 13:28:14 -0400

    Get ans from object instead of message & add comments #3

--- src/qna.py
+++ src/qna.py
@@ -1,4 +1,6 @@
-# Functionality related to Q & A
+###########################
+# Implements Q and A functionality
+###########################
 from discord import NotFound
 
 # keep track of next question number
@@ -8,7 +10,16 @@ question_number = 1
 qna = {}
 
 
-# object with question details
+###########################
+# Class: QuestionsAnswers
+# Description: object with question details
+# Inputs:
+#      - q: question text
+#      - number: question number
+#      - message: id of the message associated with question
+#      - ans: answers to the question
+# Outputs: None
+###########################
 class QuestionsAnswers:
     def __init__(self, q, number, message, ans):
         self.question = q
@@ -17,7 +28,15 @@ class QuestionsAnswers:
         self.answer = ans
 
 
-# Ask question
+###########################
+# Function: question
+# Description: takes question from user and reposts anonymously and numbered
+# Inputs:
+#      - ctx: context of the command
+#      - q: question text
+# Outputs:
+#      - User question in new post
+###########################
 async def question(ctx, q):
     global qna
     global question_number
@@ -39,7 +58,16 @@ async def question(ctx, q):
     await ctx.message.delete()
 
 
-# Answers question specified in num
+###########################
+# Function: answer
+# Description: adds user answer to specific question and post anonymously
+# Inputs:
+#      - ctx: context of the command
+#      - num: question number being answered
+#      - ans: answer text to question specified in num
+# Outputs:
+#      - User answer added to question post
+###########################
 async def answer(ctx, num, ans):
     global qna
 
@@ -74,12 +102,13 @@ async def answer(ctx, num, ans):
     q_answered.answer += new_answer
 
     # check if message exists and edit
-    content = message.content + '\n' + new_answer
+    q_str = 'Q' + str(q_answered.number) + ': ' + q_answered.question
+    content = q_str + '\n' + q_answered.answer
     try:
         await message.edit(content=content)
+        # message.content = content
     except NotFound:
         await ctx.author.send('Invalid question number: ' + str(num))
 
     # delete user msg
     await ctx.message.delete()
-
