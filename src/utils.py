052ccbc9c2f09cdb452133aa3ab69e7b78d75fe7
Alex Snezhko <alexsnezhko89@gmail.com>
2021-09-30 17:46:07 -0400

    small pylint fix in utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,8 @@
-import asyncio
-
 async def wait_for_msg(bot, channel):
     messages = await channel.history(limit=1).flatten()
     if messages[0].author.name != 'TeachersPetBot':
         return messages[0]
-        
+
     # try:
     return await bot.wait_for('message', check=lambda x: x.guild.id == channel.guild.id)
     # except asyncio.TimeoutError:
